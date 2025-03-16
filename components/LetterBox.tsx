9c12092b4c16b62cb43698192ab6806fe52ab7a3
sparsh2525 <sparsh.gupta2525@gmail.com>
2025-03-16 12:12:39 +0530

    lint fix

--- components/LetterBox.tsx
+++ components/LetterBox.tsx
@@ -10,7 +10,7 @@ interface LetterBoxProps {
     demo: boolean;
 }
 
-export default function LetterBox({ index, letter, handleChange, inputsRef, disabled, demo }: LetterBoxProps) {
+export default function LetterBox({ letter, disabled, demo }: LetterBoxProps) {
     return (
         <div
             className={`${demo ? 'w-8 h-8 text-2xl' : 'w-16 h-16 text-5xl'} ${ColourMapping[letter.status]} ${disabled ? 'select-none-all' : ''} text-slate-200 font-sans font-bold flex items-center justify-center`}
